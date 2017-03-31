90eba0da8b3500435b037e7a3cd10bc2bc619493
Noah Kiss <contact@noahkiss.com>
2017-03-31 02:54:43 -0400

    Initial commit

---
+++ netflix-skip-intro.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Netflix Skip Intro
+// @namespace    http://noahkiss.me/
+// @author       Noah Kiss
+// @description  Auto-skip intro on supported Netflix series
+// @version      0.1
+// @include      https://www.netflix.com/watch*
+// @grant        none
+// ==/UserScript==
+
+setInterval(function () {
+    'use strict';
+
+    if (!document.querySelector('div.skip-credits').classList.contains('skip-credits-hidden')) {
+        document.querySelector('a.nf-icon-button.nf-flat-button.no-icon').click();
+    }
+
+}, 4000);
