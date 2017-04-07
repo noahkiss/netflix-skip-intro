4c7b8f23639e9bcfb508f169d595430f2da7c26b
Noah Kiss <contact@noahkiss.com>
2017-04-07 03:31:07 -0400

    update version

--- netflix-skip-intro.user.js
+++ netflix-skip-intro.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://noahkiss.me/
 // @author       Noah Kiss
 // @description  Auto-skip intro on supported Netflix series
-// @version      0.1.1
+// @version      0.1.2
 // @include      https://www.netflix.com*
 // @grant        none
 // ==/UserScript==
